4da1289df0f17dc3dc5efa3f59f77e0e2c339ddd
cramyhtomit <marctimothyronquillo@gmail.com>
2023-09-24 12:19:25 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: [
+        'resources/views/**',
+        'resources/views/components/**',
+        'resources/routes/**',
+        'public/assets/**',
+        'public/assets/images/**',
+        'routes/**'
+      ],
+    }),
+  ],
+});
